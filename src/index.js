2b10d26f9486795d212a47d1462c3e64680795cd
equelzy <anton.rusilko@gmail.com>
2025-08-09 22:06:55 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const reversed = Math.abs(n).toString().split('').reverse().join('');
+  return parseInt(reversed, 10);
 };
